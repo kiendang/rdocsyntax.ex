80c9c7a2be2e0f3c0a0e9879000b7f9416e9e361
Kien Dang <mail@kien.ai>
2021-01-24 21:19:59 +0800

    Refactor

--- R/rdocsyntax.r
+++ R/rdocsyntax.r
@@ -5,14 +5,31 @@ tempf <- function(content, ...) {
   tmp
 }
 
-#' @export
-highlight_browser <- function(url) {
+
+highlight_doc <- function(url) {
   base_html_file <- system.file("index.html", package = "rdocsyntax")
   base_html <- readChar(base_html_file, file.info(base_html_file)$size)
 
   localhost_url <- sub("127.0.0.1", "localhost", url)
   html <- sub("%s", localhost_url, base_html)
-  html_file <- tempf(html, fileext = ".html")
 
-  rstudioapi::viewer(html_file)
+  tempf(html, fileext = ".html")
+}
+
+
+highlight_viewer <- function(url) {
+  rstudioapi::viewer(highlight_doc(url))
+}
+
+
+highlight_browser <- function() {
+  browser <- if (rstudioapi::isAvailable() & rstudioapi::hasFun("viewer")) {
+    highlight_viewer
+  } else getOption("browser")
+}
+
+
+#' @export
+use_highlight_browser <- function() {
+  options(browser = highlight_browser())
 }
